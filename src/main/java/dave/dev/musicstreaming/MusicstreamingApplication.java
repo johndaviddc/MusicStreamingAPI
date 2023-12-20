04c1135203965358feba0f0000d886034ca5dfc8
John David Dela Cruz <johndaviddc1@gmail.com>
2023-12-20 14:19:05 +0800

    music streaming api

---
+++ src/main/java/dave/dev/musicstreaming/MusicstreamingApplication.java
@@ -0,0 +1,13 @@
+package dave.dev.musicstreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicstreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicstreamingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dave/dev/musicstreaming/MusicstreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package dave.dev.musicstreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicstreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
